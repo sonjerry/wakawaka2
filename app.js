aea50f2b5c79e3ffb3dd60c9bce60cc2a2d6bb25
sonjerry <qh.e.720@icloud.com>
2025-09-12 01:00:15 +0900

    asd

---
+++ app.js
@@ -0,0 +1,21 @@
+const startStreaming = async () => {
+    const pc = new RTCPeerConnection();
+    const videoElement = document.getElementById('remoteVideo');
+
+    pc.ontrack = (event) => {
+        if (event.streams[0]) videoElement.srcObject = event.streams[0];
+    };
+
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+
+    const response = await fetch('http://localhost:8080/offer', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ sdp: offer.sdp, type: offer.type })
+    });
+    const answer = await response.json();
+    await pc.setRemoteDescription(new RTCSessionDescription(answer));
+};
+
+startStreaming();
